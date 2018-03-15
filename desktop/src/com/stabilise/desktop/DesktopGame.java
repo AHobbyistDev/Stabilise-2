0fab93b12c4459b3c50851cd451a54082af28a22
Are you dreaming? <weenafk@gmail.com>
2018-03-15 22:24:40 +1100

    Switched to lwjgl3 so game runs on laptop now -- not sure if it works on home computer though

--- desktop/src/com/stabilise/desktop/DesktopGame.java
+++ desktop/src/com/stabilise/desktop/DesktopGame.java
@@ -3,6 +3,8 @@ package com.stabilise.desktop;
 import com.badlogic.gdx.Files.FileType;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.stabilise.core.ResourcesRaw;
 import com.stabilise.core.main.Stabilise;
 
@@ -11,17 +13,36 @@ public class DesktopGame {
     public static void main(String[] args) {
         ResourcesRaw.loadAllDependencies();
         
-        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        // startLwjgl2(args);
+        startLwjgl3(args);
+    }
+    
+    public static void startLwjgl2(String[] args) {
+    	LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.backgroundFPS = 5;
         config.foregroundFPS = 60;
         config.title = Stabilise.GAME_NAME + " - " + Stabilise.getGameSubtitle();
         config.width = 900;
         config.height = 600;
         config.vSyncEnabled = true;
+        
+        //LwjglApplicationConfiguration.disableAudio = true;
+        
         config.addIcon("icon128.png", FileType.Classpath);
         config.addIcon("icon32.png", FileType.Classpath);
         config.addIcon("icon16.png", FileType.Classpath);
         new LwjglApplication(new Stabilise(args).getListener(), config);
+    }
+    
+    public static void startLwjgl3(String[] args) {
+    	Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+    	config.setTitle(Stabilise.GAME_NAME + " - " + Stabilise.getGameSubtitle());
+    	config.setWindowedMode(900, 600);
+    	config.useVsync(true);
+    	config.setIdleFPS(5);
+    	
+    	config.setWindowIcon(FileType.Classpath, "icon128.png", "icon32.png", "icon16.png");
+    	new Lwjgl3Application(new Stabilise(args).getListener(), config);
     }
     
 }
